2c4ac448e7c98916ec97053a63e73b56fc13bfeb
Yash <jermpro0@gmail.com>
2023-07-17 00:49:28 -0400

    Backend modules and connect script added

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify"
+import {fastifyCors } from "@fastify/cors"
+
+const server = fastify({
+    logger: true
+});
+
+server.register(fastifyCors,{
+    origin:"*",
+});
+
+server.get('/', async (req, res) => {
+    return "Hello World"
+});
+
+// const port = process.env.PORT || 5000
+
+try {
+    server.listen({ port: 5000 })
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
---
+++ server/scripts/dbConnect.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+
+export const dbConnect = () => {
+
+    try{
+
+        mongoose.connect(process.env.MONGODB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          });
+        
+        const database = mongoose.connection;
+    
+        database.on("error",(err) =>{
+            console.log(err);
+        });
+    
+        database.on("open",()=>{
+            console.log("Connected to Database");
+        });
+
+    }
+    catch(e){
+        console.log(e);
+    }
+
+}
